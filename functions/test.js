b8dd849d8cef55f745b1f0bb86d4986647c45666
Victor Monsch <victor.monsch@gmail.com>
2023-03-14 10:39:08 +0100

    Env vars

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,10 @@
 export default function handler(request, response) {
+    const CS_API_HOST = process.env.CONTENTSTACK_API_HOST;
+    const CS_CDN = process.env.CONTENTSTACK_CDN;
+    const CS_API_KEY = process.env.CONTENTSTACK_API_KEY;
+    const CS_DELIVERY_TOKEN = process.env.CONTENTSTACK_DELIVERY_TOKEN;
+    const CS_ENVIRONMENT = process.env.CONTENTSTACK_ENVIRONMENT;
+
     response.status(200).json({
         howdy: "hello",
         body: request.body,
